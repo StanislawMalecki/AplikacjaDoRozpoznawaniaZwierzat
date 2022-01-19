759ffa5fffd307c0f9ea54123d4c7d2f3194c7dd
StanisławMałecki <stas.malecki323@gmail.com>
2022-01-19 18:21:53 +0100

    new model

--- app/src/main/python/Loading test.py
+++ app/src/main/python/Loading test.py
@@ -13,25 +13,27 @@ import os
 
 def test(zdj):
     path = os.path.dirname(__file__)
-    model = keras.models.load_model(f"{path}/a/CatDogElefant.h5")
-    # model1 = keras.models.load_model(f"{path}/a/bottleneck_fc_model128.h5")
+    model = keras.models.load_model(f"{path}/a/Vgg16v2.1.h5")
+    model1 = keras.models.load_model(f"{path}/a/bottleneck_fc_model128v2.1.h5")
     pic = Image.open(io.BytesIO(bytes(zdj)))
     pic = pic.resize((128,128),Image.NEAREST)
     test_image = np.array(pic, dtype='float')
     test_image = np.expand_dims(test_image, axis = 0)
     test_image/=255
-    # result = model.predict(test_image)
-    preds = model.predict(test_image)
+    preds1 = model.predict(test_image)
+    preds = model1.predict(preds1)
     wynik = np.argmax(preds)
     if wynik == 0:
-        return "Kot " + str(preds[0][0]*100) + " %"
+        print("Kot ", preds[:wynik])
     elif wynik == 1:
-        return "Pies "+ str(preds[0][1]*100) + " %"
+        print("Koń ", preds[:wynik])
     elif wynik == 2:
-        return "Słoń "+ str(preds[0][2]*100) + " %"
-    # elif wynik == 3:
-    #     print("Kura ", preds[:wynik])
-    # elif wynik == 4:
-    #     print("Pies ", preds[:wynik])
+        print("Krowa ", preds[:wynik])
+    elif wynik == 3:
+        print("Pies ", preds[:wynik])
+    elif wynik == 4:
+        print("Sarna ", preds[:wynik])
+    elif wynik == 4:
+        print("Lis ", preds[:wynik])
     else:
-        return "Błąd"
+        print("Błąd")
